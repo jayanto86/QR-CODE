af327828e0f1add48005de37b79fe77d527c9f6c
jayanto86 <71925604+jayanto86@users.noreply.github.com>
2023-02-07 11:58:23 +0530

    Create qrcode.py

---
+++ qrcode.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import qrcode
+
+# Enter url of any website here.
+input_URL = "https://www.google.com/"
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=15,
+    border=4,
+)
+
+qr.add_data(input_URL)
+qr.make(fit=True)
+
+# convert into image
+img = qr.make_image(fill_color="black", back_color="white")
+img.save("url_qrcode.png")
+
+print(qr.data_list)
